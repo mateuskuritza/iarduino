bb91fcd031343f3f8cf0023ffb75a6f8c177aedb
mateuskuritza <mateuskuritza@gmail.com>
2025-03-30 20:16:45 -0300

    feat: load pins from list

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,19 +1,37 @@
-void setup() {
-  Serial.begin(9600);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
+const int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+const int numPins = sizeof(ledPins) / sizeof(ledPins[0]);
+
+void setup()
+{
+    Serial.begin(9600);
+
+    for (int i = 0; i < numPins; i++)
+    {
+        pinMode(ledPins[i], OUTPUT);
+    }
 }
 
-void loop() {
-  if (Serial.available()) {
-    String command = Serial.readStringUntil('\n');
-    if (command.startsWith("LED:")) {
-      int pin = command.substring(4).toInt();
-      digitalWrite(10, LOW);
-      digitalWrite(11, LOW);
-      digitalWrite(12, LOW);
-      digitalWrite(pin, HIGH);
+void loop()
+{
+    if (Serial.available())
+    {
+        String command = Serial.readStringUntil('\n');
+
+        if (command.startsWith("LED:"))
+        {
+            int pin = command.substring(4).toInt();
+
+            for (int i = 0; i < numPins; i++)
+            {
+                if (ledPins[i] == pin)
+                {
+                    digitalWrite(pin, HIGH);
+                }
+                else
+                {
+                    digitalWrite(ledPins[i], LOW);
+                }
+            }
+        }
     }
-  }
 }
